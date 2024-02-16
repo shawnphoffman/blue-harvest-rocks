5a8865839c13109e8eea8b72c8b5af567b3542ea
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-02-16 08:01:49 -0800

    Add patreon redirect

--- next.config.js
+++ next.config.js
@@ -2,4 +2,14 @@ module.exports = {
 	images: {
 		formats: ['image/avif', 'image/webp'],
 	},
+	async redirects() {
+		return [
+			{
+				source: '/patreon',
+				destination: 'https://www.patreon.com/blueharvestpodcast',
+				permanent: false,
+				basePath: false,
+			},
+		]
+	},
 }

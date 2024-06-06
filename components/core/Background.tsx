a2b8cbb045523d89d71210cb325d540c4b9140f3
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-06-06 17:59:05 -0400

    fix bh

--- components/core/Background.tsx
+++ components/core/Background.tsx
@@ -6,7 +6,7 @@ import bg from '@/app/images/raccoon.png'
 
 export default function Background() {
 	return (
-		<div className="fixed top-0 left-0 flex items-center justify-center overflow-hidden bg-black h-dvh w-dvw -z-10 bg-gradient-to-b from-black to-[#00001e]">
+		<div className="fixed top-0 left-0 flex items-center justify-center overflow-hidden  h-dvh w-dvw -z-10 bg-gradient-to-b from-black to-[#00001e]">
 			<Image
 				alt=""
 				src={bg}

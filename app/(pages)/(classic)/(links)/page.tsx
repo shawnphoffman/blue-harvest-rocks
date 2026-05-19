077169b4a43a854ef2de8f65ed29ab501b9e119f
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2026-05-19 08:22:45 -0700

    feat: use @shawnphoffman/pod-sites-shared for sanity, fetch, ratings
    Route sanity client/image/queries/types through pod-sites-shared, use shared fetchWithRetry, and consume shared Ratings components. Drop the local Ratings and AwardsScrape duplicates and add the package to the Tailwind content paths.

--- app/(pages)/(classic)/(links)/page.tsx
+++ app/(pages)/(classic)/(links)/page.tsx
@@ -1,12 +1,11 @@
+import { faStarSharp } from '@awesome.me/kit-d7ccc5bb1a/icons/classic/solid'
+import { RatingsApple, RatingsGoodpods, RatingsSpotify } from '@shawnphoffman/pod-sites-shared/ratings'
 import { Suspense } from 'react'
 
-import items from '@/app/data/links'
-import Awards from '@/components/core/AwardsScrape'
+import { getAppleReviews, getSpotifyReviews } from '@/app/data/actions'
+import items, { appleRatingUrl, goodpodsUrl, spotifyUrl } from '@/app/data/links'
 import LinkCard from '@/components/core/LinkCard'
 import Loading from '@/components/core/Loading'
-import RatingsApple from '@/components/core/RatingsApple'
-import RatingsGoodpods from '@/components/core/RatingsGoodpods'
-import RatingsSpotify from '@/components/core/RatingsSpotify'
 import Reviews from '@/components/core/Reviews'
 
 export default async function Home() {
@@ -19,14 +18,14 @@ export default async function Home() {
 
 			<div className="flex flex-row flex-wrap items-center justify-center gap-2 mb-2">
 				<Suspense fallback="">
-					<RatingsApple />
-					<RatingsGoodpods />
-					<RatingsSpotify />
+					<RatingsApple appleRatingUrl={appleRatingUrl} getReviews={getAppleReviews} starIcon={faStarSharp} />
+					<RatingsGoodpods goodpodsUrl={goodpodsUrl} starIcon={faStarSharp} />
+					<RatingsSpotify spotifyUrl={spotifyUrl} getReviews={getSpotifyReviews} starIcon={faStarSharp} />
 				</Suspense>
 			</div>
 
 			<div className="flex flex-row flex-wrap justify-center w-full gap-4 mb-4">
-				{items.map((item, i) => {
+				{items.map(item => {
 					return (
 						<LinkCard
 							key={item.title}
@@ -39,10 +38,6 @@ export default async function Home() {
 					)
 				})}
 			</div>
-
-			<Suspense>
-				<Awards />
-			</Suspense>
 
 			<Suspense fallback={<Loading />}>
 				<Reviews />
